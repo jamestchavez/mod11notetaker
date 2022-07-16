436fd6d9f0d36b3c7aa19555947ee0dbdbb854b8
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-07-16 11:20:44 -0500

    finished api/html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const router = require('express').Router();
+
+
+router.get('/notes', (req, res)=> {
+    res.sendFile(path.join(_dirname, '../public/index.html'))
+})
+
+module.exports = router;
