a227b150942969c5690c1ace301f9e072c030040
BIGCHAVO7810 <jamestchavez@gmail.com>
2022-07-17 11:55:29 -0500

    added to htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,4 +6,9 @@ router.get('/notes', (req, res)=> {
     res.sendFile(path.join(_dirname, '../public/index.html'))
 })
 
+//Send to homepage if path issue
+router.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, '../public/index.html'))
+})
+
 module.exports = router;
